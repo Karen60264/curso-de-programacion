562630eefc5af2d4ed0280ffc792ad36ab660837
Karen60264 <sonkrom82@gmail.com>
2025-07-21 12:02:22 -0400

    Add files via upload

---
+++ 0. Hola Mundo.py
@@ -0,0 +1,17 @@
+# Este es un comentario. El intérprete lo ignora.
+# La función print() muestra un mensaje en la pantalla.
+print("¡Hola, python!")
+¿cual_es_tu_nombre? 
+¿cual_es_tu_edad?
+¿cual_es_tu_pelicula_favorita?
+
+
+
+
+
+
+
+
+
+
+
