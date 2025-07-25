85de5c1679f9c75d2c7ab03db0fe1b4966ea04fd
Sonikku <sonkrom82@gmail.com>
2025-07-25 09:36:01 -0400

    Add files via upload

---
+++ NYANKO'S CAKE SHOP (con errores pero con amor).py
@@ -0,0 +1,159 @@
+
+opcion1 = "aceptar"
+opcion2 = "rechazar"
+nombre = "T/N"
+nombre2 = "Nyanko"
+
+
+opcion = input("Estabas caminando por las calles en busca de un trabajo temporal, volteas tu mirada a un cartel el cual se busca ayudante para una pasteleria que estaba a 2 cuadras")
+opcion = input("llegas al local donde se buscaba ayudante. Ves a una gatita esperando clientes hasta que te ve")
+print("Hola! bienvenid@, {}".format(nombre))
+
+opcion = input("La gatita te recibe con una amable sonrisa, luego, le explicas que venias por la solicitud de trabajo como ayudante en la pasteleria")
+print("Oh, asi que era eso, bien. Tu trabajo seria preparar los pasteles y venderlos, obviamente te ayudare en el orden de los ingredientes para que no cometas errores!")
+
+opcion = input("ya con el resumen de tu simple trabajo, debes elejir si aceptarlo o rechazarlo....¿Cual sera tu decision?")
+print("opcion1")
+print("opcion2")
+ 
+eleccion = input("ingresa el comando aceptar o rechazar: ")
+
+if eleccion == "aceptar":
+ print("¡has elegido aceptar el trabajo! Yipe!")
+if eleccion == "rechazar":
+ print("has elejido rechazar el trabajo...")
+
+#---Has aceptado el empleo! Buena suerte---
+
+opcion = input("Un cliente ha llegado, es tu momento para ganar platita de la buena hehe")
+print("Bien! llego un cliente, este ha pedido un pastel de fresa ^^")
+print("Vamos a la cocina para hacer el pastel :D")
+
+eleccion = input("Iras a la cocina o esperaras un rato?")
+
+if eleccion == "aceptar":
+ print("Bien! vamos a preparar un delicioso pastel para nuestro cliente! :D")
+if eleccion == "rechazar":
+ print("Oh..bueno, yo ire a hacerlo, quedate esperando junto al cliente TwT")
+
+#---Has llegado a la cocina, escucha bien los ingredientes y no cometas errores ;)---
+
+opcion = input("Bien, lo primero que debes poner en el molde es: Harina, azucar y 3 huevos :3")
+
+eleccion = input("Que ingredientes pondras? :D")
+
+if eleccion == "colocar la harina, azucar y 3 huevos":
+ print("Bien hecho!")
+if eleccion == "colocar cafe y aceite":
+ print("eeh, esos no son los ingredientes...")
+
+#---Siguientes ingredientes!! escucha con atencion (^^)---
+
+opcion = input("Bien! ahora, una taza de leche, mantequilla y polvo para hornear!")
+ 
+eleccion = input("Que haras a continuacion?? :v")
+
+if eleccion == "colocar leche, mantequilla y el polvo para hornear":
+ print("Yay! vas super bien!")
+if eleccion == "colocar polvora y un bloque de cemento":
+ print("...de donde salio eso????")
+
+#---Siguiente y Ultimo ingrediente!! Good Luck!! (0w0)---
+
+opcion = input("Oh!! ya es el ultimo ingrediente de la lista!! no te equivoques :D")
+opcion = input("Ahora deberas colocar la mermelada de fresa y luego la llevaremos al horno :3")
+
+eleccion = input("Bien! ultimo ingrediente...Roll or Die?")
+
+if eleccion == "colocar la mermelada de fresa":
+ print("Nice Shot!! bien hecho >///<")
+if eleccion == "colocar la mermelada de mani":
+ print("Wtf???")
+
+#---BIEN HECHO!! Ahora solo falta llevarlo al horno :D---
+
+eleccion = input("Lo llevaras al horno? ten cuidado si te quemas D:")
+
+if eleccion == "Llevarlo al horno":
+ print("Cuidado te quemas w")
+if eleccion == "Comerte la mezcla":
+ print("XD?")
+
+#---YA ESTA LISTO EL PASTEL!!---
+
+opcion = input("Ahora es momento de entregarlo al cliente :D")
+opcion = input("Entregaselo con amabilidad, este local se caracteriza por su tierna atencion al cliente :3")
+
+eleccion = input("No la kgues, como le entregaras el pastel a tu cliente??")
+
+if eleccion == "entregarlo con amabilidad y una sonrisa":
+ print("Bien hecho!!")
+if eleccion == "Tirarselo en la cara":
+ print("???")
+
+#---HAS TERMINADO TU PRIMER DIA CON UNA EXCELENTE PAGA Y BUENA ACTITUD!! Bien hecho jugador!!--
+#---Game Over--
+
+#---Este juego es corto, pero hecho con esfuerzo (y lagrimas xd)---
+#---Inspiracion: Lacey's Dinner, Bonnie's Bakery y Papa's Cupcakeria---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
